b89b9040bc7187fe2721f0d3a6068d9b1ce11b88
theactofshining <ruslan.e.valikhanov@gmail.com>
2022-04-20 17:54:21 +0200

    reworked with martin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function Article(data) {
+  return (JSON.stringify(data)
+    // <li key={user.id}>
+    //   <img src={user.picture.medium} />
+    //   <p>
+    //     {user.name.first} {user.name.last}
+    //   </p>
+    //   <div>{user.email}</div>
+    //   <div>{user.phone}</div>
+    // </li>
+  );
+}
 
 function App() {
+
+  const [articles, setArticles] = useState(false);
+
+  useEffect(() => {
+    const url = "https://hn.algolia.com/api/v1/search?query=react";
+
+    // This takes the raw response from the server and returns just the json.
+    const responseHandler = (response) => {
+      return response.json();
+    };
+
+    fetch(url)
+      .then(responseHandler)
+      .then((json) => {
+        console.log(json)
+        setArticles(json.hits)
+      }
+      )
+  },
+    []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>My Amazing Users</h1>
+
+      <div className="block">
+        In this exercise you will have to fetch the data from an API
+        <br />
+        <br />
+        Look at the instructions in <strong>index.js</strong>
+        <br />
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="link"
           target="_blank"
-          rel="noopener noreferrer"
+          href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch"
         >
-          Learn React
+          Help
         </a>
-      </header>
+      </div>
+
+      <div className="block">
+        <button onClick={() => setArticles(articles.slice(1))}>Remove first</button>
+        <ul id="users">
+          {articles && articles.map(Article)}
+        </ul>
+      </div>
     </div>
   );
 }
